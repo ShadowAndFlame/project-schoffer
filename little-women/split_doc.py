2b9387932fc89a114826c7b40a702971e1a15c3e
Jonathan Orme <jonathan.orme@quakercolor.com>
2025-11-05 15:57:22 -0500

    Split staging into parts and chapters, and delete staging

--- little-women/split_doc.py
+++ little-women/split_doc.py
@@ -1,20 +1,33 @@
 import re
+import os
 
 source_file = "source/staging.tex"
-dest_dir = "chapters"
 
-subsection_pattern = r"(?=\\subsection)"
+chapter_pattern = r"(?=\\chapter)"
 
 with open(source_file) as f:
     text = f.read()
 
-chapters = re.split(subsection_pattern, text)
+chapters = re.split(chapter_pattern, text)
 
-part = 1
 for chapter, chapter_text in enumerate(chapters):
-    if chapter == 24:
-        part = 2
-    chapter_file = f"{dest_dir}/part{part}/chapter{chapter:02d}.tex"
+    # skip empty first split
+    if not chapter_text:
+        continue
+    # Only two parts
+    part = 1 if chapter < 24 else 2
+    # Write chapter file
+    chapter_ref = f"chapters/part{part}/chapter{chapter:02d}"
+    chapter_file = chapter_ref + ".tex"
+    os.makedirs(os.path.dirname(chapter_file), exist_ok=True)
     with open(chapter_file, 'w') as f:
         f.write(chapter_text)
-    # append to parts here
+    # Write part file
+    part_ref = f"parts/part{part}"
+    part_file = part_ref + ".tex"
+    os.makedirs(os.path.dirname(part_file), exist_ok=True)
+    if not os.path.isfile(part_file):
+        with open(part_file, 'w') as f:
+            f.write("\\part{}\n\n")
+    with open(part_file, 'a') as f:
+        f.write(f"\\input{{{chapter_ref}}}\n")
